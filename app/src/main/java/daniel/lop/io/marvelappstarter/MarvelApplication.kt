9f7cbe96ce6b61430ad93078fba5da32b93e8a7c
Gabriel Andrade <gabriel.santana@gmail.com>
2023-03-18 06:28:15 -0300

    start building the adapters

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/MarvelApplication.kt
@@ -0,0 +1,9 @@
+package daniel.lop.io.marvelappstarter
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MarvelApplication : Application() {
+
+}
